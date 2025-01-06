85422451f50c3abe51b53c25de0bfb4ad342ca69
shraw kr <shrawan7070kumar@gmail.com>
2025-01-06 17:40:43 +0530

    fix: add proxy configuration for API and update postinstall script to install plugin in client directory

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  "proxy": 4000,
   "/api": {
     target: "http://image-resize-5uqz.onrender.com",
     changeOrigin: true,
