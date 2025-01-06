d722d3d36b58ac8ef76afbafe87b4cac894c1b14
shraw kr <shrawan7070kumar@gmail.com>
2025-01-06 16:02:13 +0530

    feat: update Vite configuration and dependencies for improved performance

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  "/api": {
+    target: "http://image-resize-5uqz.onrender.com",
+    changeOrigin: true,
+    secure: false,
+  },
 });
