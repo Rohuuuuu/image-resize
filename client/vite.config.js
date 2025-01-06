646c70302b60d2ffb2e5cb217b172dc93892ec1a
shraw kr <shrawan7070kumar@gmail.com>
2025-01-06 15:55:12 +0530

    feat: add Vite as a dependency and clean up Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  target: "http://localhost:10000",
-  base: "/",
   plugins: [react()],
 });
